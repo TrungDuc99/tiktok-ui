5e3738ab7240d70012c216b38d9ec08e473655ef
TrungDuc99 <dotrungduc.bl@gmail.com>
2022-05-23 10:22:06 +0700

    add logo and first code header

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,5 +1,21 @@
+import className from 'classnames/bind';
+import styles from './Header.module.scss';
+import images from '~/assets/images';
+const cx = className.bind(styles);
 function Header() {
-    return <h2>Header</h2>;
+    return (
+        <header className={cx('wrapper')}>
+            <div className={cx('inner')}>
+                <div className={cx('logo')}>
+                    <img src={images.logo} alt="Tiktok" />
+                </div>
+                <div className={cx('search')}>
+                    <input placeholder="Search accounts and videos" spellcheck={false} />
+                    <button>{/* clear */}</button>
+                </div>
+            </div>
+        </header>
+    );
 }
 
 export default Header;
