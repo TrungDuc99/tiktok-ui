06fca0f65a814a3978fd0f830aab6ecc17738a72
TrungDuc99 <dotrungduc.bl@gmail.com>
2022-05-24 01:05:05 +0700

    font awnsome icon & header input search

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,6 +1,9 @@
 import className from 'classnames/bind';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons';
 import styles from './Header.module.scss';
 import images from '~/assets/images';
+
 const cx = className.bind(styles);
 function Header() {
     return (
@@ -10,9 +13,16 @@ function Header() {
                     <img src={images.logo} alt="Tiktok" />
                 </div>
                 <div className={cx('search')}>
-                    <input placeholder="Search accounts and videos" spellcheck={false} />
-                    <button>{/* clear */}</button>
+                    <input placeholder="Search accounts and videos" spellCheck={false} />
+                    <button className={cx('clear')}>
+                        <FontAwesomeIcon icon={faCircleXmark} />
+                    </button>
+                    <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+                    <button className={cx('search-btn')}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </button>
                 </div>
+                <div className={cx('actions')}></div>
             </div>
         </header>
     );
